46bd058d8185c5bc3d162657d5aa3eba9797ffa9
Dubakrishnamohan <139947107+Dubakrishnamohan@users.noreply.github.com>
2026-03-06 00:15:59 +0530

    Update another_copy_of_welcome_to_colab.py

--- another_copy_of_welcome_to_colab.py
+++ another_copy_of_welcome_to_colab.py
@@ -7,52 +7,65 @@ mol=Chem.MolFromSmiles(Smiles)
 if mol is None:
   print("invalid smiles")
   exit()
-#------- descriptors access-------
+#<------- descriptors access------->
 mol_wt=Descriptors.MolWt(mol)
 logP=Descriptors.MolLogP(mol)
 H_Acceptors=Descriptors.NumHAcceptors(mol)
 H_Donors=Descriptors.NumHDonors(mol)
-#-------print descriptors-------
+#<-------print descriptors------->
 print("mol_wt: ",mol_wt)
 print("LogP: ",logP)
 print("H_Acceptors: ",H_Acceptors)
 print("H_Donors",H_Donors)
-fail_count= 0
+Lipsinki_fail= 0
 if(mol_wt<=500):
-    print("this molecule passes the molecular weight lipsinki rule")
+    print("lipsinki rule: pass")
 else:
     print("mol_wt: fails")
-    fail_count+=1
+    Lipsinki_fail+=1
 if(logP<=5):
      print("logP: pass")
 else:
      print("logP: fails")
-     fail_count+=1
+     Lipsinki_fail+=1
 if(H_Acceptors<=10):
       print("H_Acceptors: pass")
 else:
       print("H_Acceptors:fails")
-      fail_count+=1
+      Lipsinki_fail+=1
 if(H_Donors<=5):
       print("H_Donors :pass")
 else:
       print("H_Donors: fails")
-      fail_count+=1
+      Lipsinki_fail+=1
 
 print("\n checking Lipsinki rule.... \n")
-if fail_count<=1:
+if Lipsinki_fail<=1:
       print("lipsinki pass")
 else:
       print("lipsinki fails")
 
-#-------VEBER RULE-------
+#<-------VEBER RULE------->
 rot_bonds=Descriptors.NumRotatableBonds(mol)
 tpsa=Descriptors.TPSA(mol)
 
-#-------print descriptors-------
+#<-------print descriptors------->
 print("rot_bonds",rot_bonds)
 print("tpsa",tpsa)
+#<----checking veber rule is pass or not---->
+print("\n checking veber rule.... \n")
 if rot_bonds<=10:
   print("veber rule: pass")
 else:
   print("veber rule :fail")
+  veber_fail+=1
+if tpsa<=140:
+  print("veber rule: pass")
+else:
+  print("veber rule :fail")
+  veber_fail+=1
+veber_fail=0
+if veber_fail<=1:
+  print("veber: pass") 
+else:
+  print("veber:fail") 

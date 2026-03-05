aed83622a407857ba09fde9981d19f5d8c11a434
Dubakrishnamohan <139947107+Dubakrishnamohan@users.noreply.github.com>
2026-03-06 00:03:20 +0530

    Update another_copy_of_welcome_to_colab.py

--- another_copy_of_welcome_to_colab.py
+++ another_copy_of_welcome_to_colab.py
@@ -17,6 +17,7 @@ print("mol_wt: ",mol_wt)
 print("LogP: ",logP)
 print("H_Acceptors: ",H_Acceptors)
 print("H_Donors",H_Donors)
+Print("\n checking Lipsinki rule.... \n") 
 fail_count= 0
 if(mol_wt<=500):
     print("this molecule passes the molecular weight lipsinki rule")
@@ -24,17 +25,17 @@ else:
     print("mol_wt: fails")
     fail_count+=1
 if(logP<=5):
-     print("this molecule passes the logP lipsinki rule")
+     print("logP: pass")
 else:
      print("logP: fails")
      fail_count+=1
 if(H_Acceptors<=10):
-      print("this molecule passes the H_Acceptors lipsinki rule")
+      print("H_Acceptors: pass")
 else:
       print("H_Acceptors:fails")
       fail_count+=1
 if(H_Donors<=5):
-      print("this molecule passes the H_Donors lipsinki rule")
+      print("H_Donors :pass")
 else:
       print("H_Donors: fails")
       fail_count+=1
@@ -51,6 +52,6 @@ tpsa=Descriptors.TPSA(mol)
 print("rot_bonds",rot_bonds)
 print("tpsa",tpsa)
 if rot_bonds<=10:
-  print("this molecule follows vebers rule")
+  print("veber rule: pass")
 else:
-  print("this molecule doesnot follows vebers rule")
+  print("veber rule :fail")

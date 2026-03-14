fbb6b4134f0b40c4ace4b0627dd2d4db58d2d760
Dubakrishnamohan <139947107+Dubakrishnamohan@users.noreply.github.com>
2026-03-14 18:41:24 +0530

    Update another_copy_of_welcome_to_colab.py

--- another_copy_of_welcome_to_colab.py
+++ another_copy_of_welcome_to_colab.py
@@ -1,28 +1,37 @@
 from rdkit import Chem
 from rdkit.Chem import Draw
+from rdkit.Chem import ALLChem
 from rdkit.Chem import Descriptors
 import pandas as pd
 #<-------2. Load Dataset------->
 file=input("enter csv file name: ")
 df=pd.read_csv(file)
 #<-------3. Initialize Result Storage------->
+molwt_list=[]
+logP_list=[]
+H_Acceptors_list=[]
+H_Donors_list=[]
+rot_bonds_list=[] 
+tpsa_list=[] 
 lipinski_results=[]
 veber_results=[]
 lipinski_fails=[]
 veber_fails=[]
 #<-------4. Process Each Molecule------->
 for i in range(len(df)):
-  Smiles=df["SMILES"][i]
+  Smiles=df.loc[i,"SMILES"]
     #<-------4.1 Convert Smiles Into Molecules------->
   mol=Chem.MolFromSmiles(Smiles)
   if mol is None:
-     print("invalid smiles")
+     lipinski_results.append("invalid")
+     veber_results.append("invalid")
+     lipinski_fails.append(None)
+     veber_fails.append(None)
      continue
 
     #<-------4.2 Calculate Molecular Descriptors ------->
   mol_wt=Descriptors.MolWt(mol)
-  logP=Descriptors.MolLogP(mol)
-  H_Acceptors=Descriptors.NumHAcceptors(mol)
+  logP=Descriptors.MolLogP(mol)        H_Acceptors=Descriptors.NumHAcceptors(mol)
   H_Donors=Descriptors.NumHDonors(mol)
     #<-------5. Apply Lipinski Rule Of Five ------->
 
@@ -53,17 +62,27 @@ for i in range(len(df)):
   else:
     veber_result="fail"
   #<-------8. store results------->
+  molwt_list.append(molwt_list)
+  logP_list.append(logP_list)
+  H_acceptors.append(H_Acceptors_list)
+  H_Donors.append(H_Donors_list)
+  rot_bonds.append(rot_bonds_list)
+  tpsa.append(tpsa_list)
   lipinski_results.append(lipinski_result) 
   veber_results.append(veber_result)
   lipinski_fails.append(lipinski_fail)
   veber_fails.append(veber_fail)
     #<-------9. Add Results To Dataset------->
+df["mol_wt"]=molwt_list
+df["logP"]=logp_list
+df["H_Acceptors"]=H_Acceptors_list
+df["H_Donors"]=H_Donors_list
+df["rot_bonds"]=rot_bonds_list
+df["TPSA"]=tpsa_list
 df["lipinski_results"]=lipinski_results
 df["veber_results"]=veber_results
 df["lipinski_fails"] =lipinski_fails
 df["veber_Fails"]=veber_fails
-
-
       #<-------10. Save Updated Dataset------->
-df.to_csv("paracetmol.data.csv",index=False)
-print("updated dataset saved")
+df.to_csv("drug_analysis_results.csv",index=False)
+print("updated drug analysis dataset saved")
